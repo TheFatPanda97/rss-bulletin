f4e0ea43e067a3dafa6603485306e3abd2d56f0a
TheFatPanda97 <shawnhu97@gmail.com>
2020-12-09 15:28:14 -0500

    testing firestore

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,7 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
 
 exports.byeWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Bye logs!", {structuredData: true});
-    response.send("Bye from Firebase!");
-  });
-  
+	functions.logger.info("Bye logs!", { structuredData: true });
+	response.send("Bye from Firebase!");
+});
+
+exports.createDoc = functions.https.onRequest(async (req, res) => {
+	const docRef = db.collection('users').doc('alovelace');
+    await docRef.set({
+    first: 'Serena',
+    last: 'Yu',
+    born: 1995
+    });
+    res.send("document created")
+});
