101ea8fe2f9594d10d5fe9c7a9c8da3805a81f3a
TheFatPanda97 <shawnhu97@gmail.com>
2020-12-13 15:28:48 -0500

    trying out bcrypt

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,22 +1,40 @@
 <template>
-	<div>
-		<section id="firebaseui-auth-container"></section>
-	</div>
+    <div>
+        <section id="firebaseui-auth-container"></section>
+        <p>{{ secret }}</p>
+        <button @click="generate()">generate hash</button>
+    </div>
 </template>
 
 <script>
-import firebase from "firebase/app"
-import * as firebaseui from "firebaseui" 
-import "firebase/auth"
+import firebase from "firebase/app";
+import * as firebaseui from "firebaseui";
+import "firebase/auth";
 import "firebaseui/dist/firebaseui.css";
+import bcrypt from "bcryptjs";
 
 export default {
-	mounted() {
-		let ui = new firebaseui.auth.AuthUI(firebase.auth());
-		ui.start("#firebaseui-auth-container", {
+    data() {
+        return {
+            secret: "",
+        };
+    },
+    mounted() {
+        let ui = new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start("#firebaseui-auth-container", {
             signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-            signInSuccessUrl: "/about"
-		});
-	},
+            signInSuccessUrl: "/about",
+        });
+    },
+    methods: {
+        generate() {
+            bcrypt.hash("12345", 10, (err, hash) => {
+                this.secret = hash;
+                bcrypt.compare("12345", hash, function(err, res) {
+                    console.log(res);
+                });
+            });
+        },
+    },
 };
 </script>
