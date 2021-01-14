25fe185ceac2b7d83e722d9ca4c5f35fa15b14d6
TheFatPanda97 <shawnhu97@gmail.com>
2021-01-13 22:19:25 -0500

    small bug fixes

--- app/src/components/EventCard.vue
+++ app/src/components/EventCard.vue
@@ -1,41 +1,61 @@
 <template>
-	<v-hover v-slot="{ hover }">
-		<v-card
-			:elevation="hover ? 6 : 2"
-			class="transition-swing"
-			style="cursor:pointer"
-			@click="goToPage"
-			height="100%"
-		>
-			<v-img
-				class="white--text align-end"
-				height="200px"
-				:src="imgSrc"
-				style="position: relative"
+	<div style="height: 100%">
+		<v-hover v-slot="{ hover }">
+			<v-card
+				:elevation="hover ? 6 : 2"
+				class="transition-swing"
+				style="cursor:pointer"
+				height="100%"
 			>
-				<v-btn
-					fab
-					small
-					dark
-					color="#5B7CF0"
-					style="position: absolute; bottom: 10px; right: 10px"
+				<v-img
+					class="white--text align-end"
+					height="200px"
+					@click="goToPage"
+					:src="imgSrc"
+					style="position: relative"
 				>
-					<v-icon>
-						mdi-heart-outline
-					</v-icon>
+					<v-btn
+						@click.stop="test"
+						fab
+						small
+						dark
+						color="#5B7CF0"
+						style="position: absolute; bottom: 10px; right: 10px; z-index: 10"
+					>
+						<v-icon>
+							mdi-heart-outline
+						</v-icon>
+					</v-btn>
+				</v-img>
+
+				<div @click="goToPage">
+					<v-card-subtitle class="font-weight-bold pb-0" style="color: #5B7CF0">
+						{{ date }} {{ time }}
+					</v-card-subtitle>
+					<v-card-title class="mb-2 pt-1" style="word-break: normal">
+						{{ title }}
+					</v-card-title>
+				</div>
+			</v-card>
+		</v-hover>
+		<v-snackbar class="snackbar-wrapper" v-model="snackbar" absolute>
+			Still working on it 😅, stay tuned!
+
+			<template v-slot:action="{ attrs }">
+				<v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+					Close
 				</v-btn>
-			</v-img>
-			<v-card-subtitle class="font-weight-bold pb-0" style="color: #5B7CF0">
-				{{ date }} {{ time }}
-			</v-card-subtitle>
-			<v-card-title class="mb-2 pt-1" style="word-break: normal">
-				{{ title }}
-			</v-card-title>
-		</v-card>
-	</v-hover>
+			</template>
+		</v-snackbar>
+	</div>
 </template>
 <script>
 export default {
+	data() {
+		return {
+			snackbar: false,
+		};
+	},
 	props: {
 		title: String,
 		imgSrc: String,
@@ -50,6 +70,9 @@ export default {
 				name: "Event",
 				params: { id: this.id, category: this.category },
 			});
+		},
+		test() {
+			this.snackbar = true;
 		},
 	},
 };
--- app/src/views/Event.vue
+++ app/src/views/Event.vue
@@ -1,98 +1,120 @@
 <template>
-    <v-container>
-        <v-card v-if="eventDetail">
-            <v-container class="pt-0">
-                <v-row style="background-color: #FCFBFB">
-                    <v-col class="pa-0" cols="12" md="8">
-                        <v-img
-                            :src="
-                                eventDetail.imgSrc
-                                    ? eventDetail.imgSrc
-                                    : '../assets/load.png'
-                            "
-                            lazy-src="../assets/load.png"
-                            height="350"
-                        >
-                            <template v-slot:placeholder>
-                                <v-row
-                                    class="fill-height ma-0"
-                                    align="center"
-                                    justify="center"
-                                >
-                                    <v-progress-circular
-                                        indeterminate
-                                        color="grey lighten-5"
-                                    ></v-progress-circular>
-                                </v-row>
-                            </template>
-                        </v-img>
-                    </v-col>
-                    <v-col cols="12" md="4" style="position: relative; min-height:250px">
-                        <v-col>
-                            <p>
-                                {{ eventDetail.date }}
-                            </p>
-                            <p class="text-subtitle-1 font-weight-medium">
-                                {{ eventDetail.title }}
-                            </p>
-                            <p>Location: {{ eventDetail.location }}</p>
-                        </v-col>
-                        <v-col style="position: absolute; bottom: 0; left: 0">
-                            <v-btn block dark color="#0E8548">
-                                Save
-                            </v-btn>
-                        </v-col>
-                    </v-col>
-                </v-row>
-                <v-row style="border-top: 0.1px solid grey;">
-                    <v-col>
-                        {{ eventDetail.description }}
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-card>
-        <p v-else>loading</p>
-    </v-container>
+	<v-container>
+		<v-card v-if="eventDetail">
+			<v-container class="pt-0">
+				<v-row style="background-color: #FCFBFB">
+					<v-col class="pa-0" cols="12" md="8">
+						<v-img
+							:src="
+								eventDetail.imgSrc
+									? eventDetail.imgSrc
+									: '../assets/load.png'
+							"
+							lazy-src="../assets/load.png"
+							height="350"
+						>
+							<template v-slot:placeholder>
+								<v-row
+									class="fill-height ma-0"
+									align="center"
+									justify="center"
+								>
+									<v-progress-circular
+										indeterminate
+										color="grey lighten-5"
+									></v-progress-circular>
+								</v-row>
+							</template>
+						</v-img>
+					</v-col>
+					<v-col cols="12" md="4" style="position: relative; min-height:250px">
+						<v-col>
+							<p>
+								{{ eventDetail.date }}
+							</p>
+							<p class="text-subtitle-1 font-weight-medium">
+								{{ eventDetail.title }}
+							</p>
+							<p>Location: {{ eventDetail.location }}</p>
+						</v-col>
+						<v-col style="position: absolute; bottom: 0; left: 0">
+							<v-btn block dark color="#0E8548" @click="test">
+								Save
+							</v-btn>
+						</v-col>
+					</v-col>
+				</v-row>
+				<v-row style="border-top: 0.1px solid grey;">
+					<v-col>
+						{{ eventDetail.description }}
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-card>
+		<v-skeleton-loader
+			v-else
+			type="card-avatar, article, actions"
+		></v-skeleton-loader>
+		<v-snackbar class="snackbar-wrapper" v-model="snackbar" absolute>
+			Still working on it 😅, stay tuned!
+
+			<template v-slot:action="{ attrs }">
+				<v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+					Close
+				</v-btn>
+			</template>
+		</v-snackbar>
+	</v-container>
 </template>
 <script>
 export default {
-    async created() {
-        let tempEvent;
-        switch (this.$route.params.category) {
-            case "general":
-                if (this.$store.state.general.length === 0) {
-                    await this.$store.dispatch("bindGeneral");
-                }
+	data() {
+		return {
+			snackbar: false,
+		};
+	},
+	methods: {
+		test() {
+			this.snackbar = true;
+		},
+	},
+	async created() {
+		let tempEvent;
+		switch (this.$route.params.category) {
+			case "general":
+				if (this.$store.state.general.length === 0) {
+					await this.$store.dispatch("bindGeneral");
+				}
 
-                tempEvent = this.$store.state.general.find(
-                    (event) => event.id === this.$route.params.id
-                );
+				tempEvent = this.$store.state.general.find(
+					(event) => event.id === this.$route.params.id
+				);
 
-                if (!tempEvent) this.$router.push({ name: "Home" });
+				if (!tempEvent) this.$router.push({ name: "Home" });
 
-                break;
-            default:
-                this.$router.push({ name: "Home" });
-                break;
-        }
-    },
-    computed: {
-        eventDetail() {
-            return this.$store.state.general.find(
-                (event) => event.id === this.$route.params.id
-            );
-        },
-    },
-    watch: {
-        "$store.state.general": function (newVal) {
-            if (newVal.length != 0) {
-                let tempEvent = this.$store.state.general.find(
-                    (event) => event.id === this.$route.params.id
-                );
+				break;
+			default:
+				this.$router.push({ name: "Home" });
+				break;
+		}
+	},
+	computed: {
+		eventDetail() {
+			return this.$store.state.general.find(
+				(event) => event.id === this.$route.params.id
+			);
+		},
+	},
+	watch: {
+		"$store.state.general": function(newVal) {
+			if (newVal.length != 0) {
+				let tempEvent = this.$store.state.general.find(
+					(event) => event.id === this.$route.params.id
+				);
 
-                if (!tempEvent) this.$router.push({ name: "Home" });
-            }
-        },
-    },
+				if (!tempEvent) this.$router.push({ name: "Home" });
+			}
+		},
+	},
 };
 </script>
